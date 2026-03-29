0d48b20b31653e9fc0a69f33c1900db531e5b338
b1329020 <kevinq22345@gmail.com>
2026-03-29 16:50:14 +0800

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class table(models.Model):
-    Department = models.TextField()
-    CourseTitle = models.TextField()
-    Instructor = models.TextField()
