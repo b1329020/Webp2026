4956af5c753239e7480019a29b94ffd836a1ad80
b1329020 <kevinq22345@gmail.com>
2026-05-23 00:11:32 +0800

    Add files via upload

---
+++ ex12/react_login/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Login'; // 引入你剛剛建立的 Login.js
+
+function App() {
+  return (
+    <div>
+      {/* 渲染 MUI v5 的登入頁面模板 */}
+      <Login />
+    </div>
+  );
+};
+
+export default App;
